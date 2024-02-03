10ff488cc3004caaad2a862751a5cbaa6dd898e2
ClavinK <143671527+ClavinK@users.noreply.github.com>
2024-02-03 13:49:42 -0800

    Update studySpaces.js

--- studySpaces.js
+++ studySpaces.js
@@ -1,4 +1,3 @@
 document.getElementById('mapsButton').addEventListener('click', function() {
-    var mapContainer = document.getElementById('map');
-    mapContainer.style.display = mapContainer.style.display === 'none' ? 'block' : 'none';
+    window.location.href = 'maps.html';
 });
